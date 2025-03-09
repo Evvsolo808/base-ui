4f3bb1fd3dbf42668a9aa0460d2e6b842c13ce8b
Evvsolo808 <soloevv808@gmail.com>
2025-03-08 20:04:07 -0500

    Update index.parts.ts

--- packages/react/src/tooltip/index.parts.ts
+++
@@ -1,7 +0,0 @@
-export { TooltipRoot as Root } from './root/TooltipRoot';
-export { TooltipTrigger as Trigger } from './trigger/TooltipTrigger';
-export { Portal } from '../portal/Portal';
-export { TooltipPositioner as Positioner } from './positioner/TooltipPositioner';
-export { TooltipPopup as Popup } from './popup/TooltipPopup';
-export { TooltipArrow as Arrow } from './arrow/TooltipArrow';
-export { TooltipProvider as Provider } from './provider/TooltipProvider';
